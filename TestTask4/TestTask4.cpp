69ede8eef49c99c524da95848cd50f79a4b34b54
IIvan8 <vanya.van28@mail.ru>
2025-05-26 14:38:56 +0300

    Выполнил задание 4

---
+++ TestTask4/TestTask4.cpp
@@ -0,0 +1,128 @@
+﻿#include "CppUnitTest.h"
+#include "../decisionTask4/PriorityQueue.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace PriorityQueueTests
+{
+    TEST_CLASS(PriorityQueueTests)
+    {
+    public:
+
+        TEST_METHOD(DefaultConstructor_CreatesEmptyQueue)
+        {
+            PriorityQueue queue;
+
+            Assert::IsTrue(queue.isEmpty());
+            Assert::AreEqual(size_t(0), queue.getSize());
+        }
+
+        TEST_METHOD(InitializerListConstructor_CreatesQueueWithElements)
+        {
+            PriorityQueue queue = { {10, 3}, {20, 1}, {30, 5} };
+
+            Assert::AreEqual(size_t(3), queue.getSize());
+            Assert::AreEqual(30, queue.findMax());
+        }
+
+        TEST_METHOD(Insert_AddsElementWithPriority)
+        {
+            PriorityQueue queue;
+
+            queue.insert(10, 2);
+            queue.insert(20, 1);
+            queue.insert(30, 3);
+
+            Assert::AreEqual(size_t(3), queue.getSize());
+            Assert::AreEqual(30, queue.findMax());
+        }
+
+        TEST_METHOD(FindMax_ReturnsHighestPriorityElement)
+        {
+            PriorityQueue queue = { {10, 3}, {20, 5}, {30, 1} };
+
+            Assert::AreEqual(20, queue.findMax());
+        }
+
+        TEST_METHOD(FindMax_OnEmptyQueue_ThrowsException)
+        {
+            PriorityQueue queue;
+
+            Assert::ExpectException<std::runtime_error>([&]() { queue.findMax(); });
+        }
+
+        TEST_METHOD(ExtractMax_RemovesAndReturnsHighestPriorityElement)
+        {
+            PriorityQueue queue = { {10, 3}, {20, 5}, {30, 1} };
+
+            Assert::AreEqual(20, queue.extractMax());
+            Assert::AreEqual(size_t(2), queue.getSize());
+            Assert::AreEqual(10, queue.findMax());
+        }
+
+        TEST_METHOD(ExtractMax_OnEmptyQueue_ThrowsException)
+        {
+            PriorityQueue queue;
+
+            Assert::ExpectException<std::runtime_error>([&]() { queue.extractMax(); });
+        }
+
+        TEST_METHOD(IsEmpty_ReturnsTrueForEmptyQueue)
+        {
+            PriorityQueue queue;
+
+            Assert::IsTrue(queue.isEmpty());
+        }
+
+        TEST_METHOD(IsEmpty_ReturnsFalseForNonEmptyQueue)
+        {
+            PriorityQueue queue = { {10, 1} };
+
+            Assert::IsFalse(queue.isEmpty());
+        }
+
+        TEST_METHOD(CopyConstructor_CreatesIndependentCopy)
+        {
+            PriorityQueue original = { {10, 3}, {20, 1} };
+
+            PriorityQueue copy(original);
+            original.extractMax();
+
+            Assert::AreEqual(size_t(1), original.getSize());
+            Assert::AreEqual(size_t(2), copy.getSize());
+            Assert::AreEqual(10, copy.findMax());
+        }
+
+        TEST_METHOD(MoveConstructor_TransfersOwnership)
+        {
+            PriorityQueue original = { {10, 3}, {20, 1} };
+
+            PriorityQueue moved(std::move(original));
+
+            Assert::AreEqual(size_t(2), moved.getSize());
+            Assert::AreEqual(size_t(0), original.getSize());
+            Assert::IsTrue(original.isEmpty());
+        }
+
+        TEST_METHOD(ShiftLeftOperator_InsertsElement)
+        {
+            PriorityQueue queue;
+
+            queue << std::make_pair(10, 2) << std::make_pair(20, 3);
+
+            Assert::AreEqual(size_t(2), queue.getSize());
+            Assert::AreEqual(20, queue.findMax());
+        }
+
+        TEST_METHOD(ShiftRightOperator_ExtractsElement)
+        {
+            PriorityQueue queue = { {10, 3}, {20, 1} };
+            int value = 0;
+
+            queue >> value;
+
+            Assert::AreEqual(10, value);
+            Assert::AreEqual(size_t(1), queue.getSize());
+        }
+    };
+}
---
+++ decisionTask4/PriorityQueue.h
@@ -0,0 +1,117 @@
+﻿#pragma once
+#include <initializer_list>
+#include <utility> 
+
+class PriorityQueue {
+private:
+    struct Node {
+        int value;
+        int priority;
+        Node* next;
+
+        Node(int val, int prio) : value(val), priority(prio), next(nullptr) {}
+    };
+
+    Node* head;
+    size_t size;
+public:
+    /**
+    * @brief Конструктор по умолчанию. Создает пустую очередь.
+    */
+    PriorityQueue();
+
+    /**
+    * @brief Конструктор с инициализацией из списка
+    * @param initList Список пар {значение, приоритет} для инициализации
+    */
+    PriorityQueue(std::initializer_list<std::pair<int, int>> initList);
+
+    /**
+    * @brief Конструктор копирования
+    * @param other Очередь для копирования
+    */
+    PriorityQueue(const PriorityQueue& other);
+
+    /**
+    * @brief Конструктор перемещения
+    * @param other Очередь для перемещения
+    */
+    PriorityQueue(PriorityQueue&& other) noexcept;
+
+    /**
+    * @brief Деструктор. Очищает память от всех узлов.
+    */
+    ~PriorityQueue();
+
+    /**
+    * @brief Оператор копирующего присваивания
+    * @param other Очередь для копирования
+    * @return Ссылка на текущую очередь
+    */
+    PriorityQueue& operator=(const PriorityQueue& other);
+
+    /**
+    * @brief Оператор перемещающего присваивания
+    * @param other Очередь для перемещения
+    * @return Ссылка на текущую очередь
+    */
+    PriorityQueue& operator=(PriorityQueue&& other) noexcept;
+
+    /**
+    * @brief Добавляет элемент в очередь
+    * @param value Значение элемента
+    * @param priority Приоритет элемента
+    */
+    void insert(int value, int priority);
+
+    /**
+    * @brief Извлекает элемент с максимальным приоритетом
+    * @return Значение элемента с наивысшим приоритетом
+    */
+    int extractMax();
+
+    /**
+    * @brief Возвращает элемент с максимальным приоритетом без извлечения
+    * @return Значение элемента с наивысшим приоритетом
+    */
+    int findMax() const;
+
+    /**
+    * @brief Проверяет, пуста ли очередь
+    * @return true если очередь пуста, false иначе
+    */
+    bool isEmpty() const;
+
+    /**
+    * @brief Возвращает размер очереди
+    * @return Количество элементов в очереди
+    */
+    size_t getSize() const;
+
+    /**
+    * @brief Преобразует очередь в строку (для отладки)
+    * @return Строковое представление очереди
+    */
+    std::string toString() const;
+
+    /**
+    * @brief Очищает очередь
+    */
+    void clear();
+};
+
+/**
+* @brief Оператор вставки элемента в очередь
+* @param queue Очередь для вставки
+* @param element Пара {значение, приоритет}
+* @return Ссылка на измененную очередь
+*/
+PriorityQueue& operator<<(PriorityQueue& queue, const std::pair<int, int>& element);
+
+/**
+* @brief Оператор извлечения элемента из очереди
+* @param queue Очередь для извлечения
+* @param value Переменная для записи извлеченного значения
+* @return Ссылка на измененную очередь
+*/
+PriorityQueue& operator>>(PriorityQueue& queue, int& value);
